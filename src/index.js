bff1384780c3c0a3de2d63f50790c662a1ee5ad8
Kharkob <146363186+Kharkob@users.noreply.github.com>
2024-01-22 12:14:17 -0800

    push

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import NewsFeed from './components/news-feed';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NewsFeed />
   </React.StrictMode>
 );
 
